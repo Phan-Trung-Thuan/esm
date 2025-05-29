c529a03bbd9c37f7f542c23e66a79f7ca91afd91
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2025-05-29 19:32:07 +0800

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -85,8 +85,8 @@ def run(args):
     assert all(-(model.num_layers + 1) <= i <= model.num_layers for i in args.repr_layers)
     repr_layers = [(i + model.num_layers + 1) % (model.num_layers + 1) for i in args.repr_layers]
 
-    start_batch = 500
-    end_batch = 700
+    start_batch = 700
+    end_batch = 900
     with torch.no_grad():
         for batch_idx, (labels, strs, toks) in tqdm(enumerate(data_loader)):
             if start_batch <= batch_idx and batch_idx < end_batch: 
