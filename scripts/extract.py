f4e7eabc70e9c45bb77118f17bd6146ea538f621
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2025-06-24 19:28:23 +0800

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -109,10 +109,10 @@ def run(args):
                     contacts = out["contacts"].to(device="cpu")
     
                 for i, label in enumerate(labels):
-                    label = label.split("|")[1]
-                    args.output_file = args.output_dir / f"{label}.pt"
+                    id, label = label.split(" ")
+                    args.output_file = args.output_dir / f"{id}_{label}.pt"
                     args.output_file.parent.mkdir(parents=True, exist_ok=True)
-                    result = {"label": label}
+                    result = {"id": id, "label": label}
                     truncate_len = min(args.truncation_seq_length, len(strs[i]))
                     # Call clone on tensors to ensure tensors are not views into a larger representation
                     # See https://github.com/pytorch/pytorch/issues/1995
